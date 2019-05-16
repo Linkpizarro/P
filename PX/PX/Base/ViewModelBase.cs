31bfe2a46dd60d7e4b413181e4febc04d3945623
Linkpizarro <elcentaleon@gmail.com>
2019-05-16 11:33:03 +0200

    Base -> ViewModelBase

---
+++ PX/PX/Base/ViewModelBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PX.Base
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChange(String propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
