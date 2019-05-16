c342882d384972123e6fc2101d9ec01f10b7ade6
Linkpizarro <elcentaleon@gmail.com>
2019-05-16 11:22:29 +0200

    Tools->Api

---
+++ PX/PX/Tools/Api.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PX.Tools
+{
+    public class Api
+    {
+        public static String uriapi = "https://tiendaapi.azurewebsites.net/";
+        public static MediaTypeWithQualityHeaderValue headerjson = new MediaTypeWithQualityHeaderValue("application/json");
+
+        public static async Task<T> CallApi<T>(String peticion, String token)
+        {
+            using (HttpClient cliente = new HttpClient())
+            {
+                cliente.BaseAddress = new Uri(uriapi);
+                cliente.DefaultRequestHeaders.Accept.Clear();
+                cliente.DefaultRequestHeaders.Accept.Add(headerjson);
+                if (token != null)
+                {
+                    cliente.DefaultRequestHeaders.Add("Authorization", "bearer "
+                        + token);
+                }
+                HttpResponseMessage response = await cliente.GetAsync(peticion);
+                if (response.IsSuccessStatusCode)
+                {
+                    String content = await response.Content.ReadAsStringAsync();
+                    return JsonConvert.DeserializeObject<T>(content);
+                }
+                return default(T);
+            }
+        }
+        public static async Task PostApi(Object obj, String peticion, String token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(uriapi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(headerjson);
+                if (token != null)
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "bearer "
+                        + token);
+                }
+                String stringJson = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(stringJson, Encoding.UTF8, "application/json");
+                HttpResponseMessage responseMessage = await client.PostAsync(peticion, content);
+            }
+        }
+        public static async Task PutApi(Object obj, String peticion, String token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(uriapi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(headerjson);
+                if (token != null)
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "bearer "
+                        + token);
+                }
+                String stringJson = JsonConvert.SerializeObject(obj);
+                StringContent content = new StringContent(stringJson, Encoding.UTF8, "application/json");
+                HttpResponseMessage responseMessage = await client.PutAsync(peticion, content);
+            }
+        }
+        public static async Task DeleteApi(String peticion, String token)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(uriapi);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(headerjson);
+                if (token != null)
+                {
+                    client.DefaultRequestHeaders.Add("Authorization", "bearer "
+                        + token);
+                }
+                HttpResponseMessage responseMessage = await client.DeleteAsync(peticion);
+            }
+        }
+    }
+}
