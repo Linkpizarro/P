91d41ee4cb1fba0d2995b291c191b00951838ac4
Linkpizarro <elcentaleon@gmail.com>
2019-05-17 09:14:22 +0200

    menu -> Home,Productos,Login

--- PX/PX/Views/NavbarView.xaml.cs
+++ PX/PX/Views/NavbarView.xaml.cs
@@ -23,14 +23,25 @@ namespace PX.Views
                 BarBackgroundColor = Color.FromHex("#4528A2")
             };
             IsPresented = false;
+            this.Menu.ItemSelected += ChangePage;
         }
         private void LoadPages()
         {
             var home = new ItemMenuPage() { Title = "Home",TypePage= typeof(MainPage) };
+            var products = new ItemMenuPage() { Title = "Productos",TypePage=typeof(ProductosView)};
             var login = new ItemMenuPage() { Title = "Login", TypePage = typeof(LoginView) };
             this.Items.Add(home);
             this.Items.Add(login);
+            this.Items.Add(products);
             this.Menu.ItemsSource = this.Items;
+        }
+        private void ChangePage(object sender, SelectedItemChangedEventArgs e)
+        {
+            ItemMenuPage select = (ItemMenuPage)e.SelectedItem;
+            Detail = new NavigationPage((Page)Activator.CreateInstance(select.TypePage)) {
+                BarBackgroundColor = Color.FromHex("#4528A2")
+            };
+            IsPresented = false;
         }
     }
 }
