ee21d599df2e7998428e27cb205b8a448e65f5ab
Andrés Marcelo Sánchez Luzuriaga <Andres.Sanchez.Luzuriaga@tajamar365.com>
2019-05-23 14:30:11 +0200

    Cambios en ConvertidorSubtotal.cs

--- PX/PX/Converters/ConvertidorSubtotal.cs
+++ PX/PX/Converters/ConvertidorSubtotal.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PX.Models;
+using PX.Services;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -10,34 +12,65 @@ namespace PX.Converters
     public class ConvertidorSubtotal : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {           
+        {
+            //if (value != null)
+            //{
+            //    if (value.ToString() != null)
+            //    {
+            //        decimal subtotal = 0;
+            //        //string resultado = string.Empty;
+            //        int cantidad = int.Parse(value.ToString());
+            //        //subtotal = cantidad * precio;
+            //        //subtotal = (decimal)value * (int)parameter;
+            //        subtotal = cantidad * 2; //NOTA: FUNCIONA SIN "parameter"
+
+            //        //subtotal = cantidad * int.Parse(((Label)parameter).Text);
+            //        //subtotal = cantidad * int.Parse(parameter.ToString());
+
+            //        return subtotal;                    
+            //    }
+            //    else
+            //    {
+            //        return 0;
+            //    }                
+            //}
+            //else
+            //{
+            //    return "";
+            //}
+
+
+            //ENLACES DE INTERES: 
+            //https://stackoverflow.com/questions/37647893/xamarin-forms-converterparameter-how-to-reference-to-current-item-property
+            //https://forums.xamarin.com/discussion/71810/pass-binding-to-converterparameter
             if (value != null)
             {
-                if (value.ToString() != null)
-                {
-                    decimal _Subtotal = 0;
-                    string resultado = string.Empty;
-                    _Subtotal = decimal.Parse(value.ToString());
-                    //_Subtotal = cantidad * precio;
-                    //_Subtotal = (decimal)value * (int)parameter;
-                    _Subtotal = _Subtotal * 2; //NOTA: FUNCIONA SIN "parameter"
+                //if (value.ToString() != null)
+                //{
+                    decimal subtotal = 0;
 
-                    //_Subtotal = _Subtotal * int.Parse(((Label)parameter).Text);
-                    //_Subtotal = _Subtotal * int.Parse(parameter.ToString());
+                    int cantidad = int.Parse(((Label)parameter).Text);
+                    int precio = ((Producto)value).PrecioUnidad;
 
-                    return _Subtotal;
-                    
-                }
-                else
-                {
-                    return 0;
-                }                
+                    subtotal = cantidad * precio;
+
+                    //Producto prod = value as Producto; //NOTA: NO FUNCIONA PORQUE NO SE PASA EL OBJETO
+                    //SessionService session = App.Locator.SessionService;
+                    //int indice = session.Articulos.IndexOf(prod);
+                    //session.Articulos[indice].Subtotal = subtotal;
+
+                    return subtotal;
+                //}
+                //else
+                //{
+                //    return 0;
+                //}
             }
             else
             {
                 return "";
             }
-           
+
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
